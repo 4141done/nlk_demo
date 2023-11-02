0e5f9d1d5082c26e94537f9902d3a36e1e7bccc0
Javier Evans <j.evans@f5.com>
2023-11-02 14:31:04 -0700

    partially working cluster internal prometheus, improvements to commands.md

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
-
-echo "Remember to run the following before running this script.  If you forgot, press ^c and do it now..."
-echo "kind create cluster --config kind/3node-config.yaml"
-echo "docker compose up"
-
+kind delete cluster --name nlk-multi-node-demo
+docker compose up -d
+kind create cluster --config kind/3node-config.yaml
 sleep 10
-
-echo "Sounds like you are ready, setting up..."
+kubectl cluster-info --context kind-nlk-multi-node-demo
+echo "Aww yeahh"
 
 ## Make sure the config-map.yaml is the expected value for templating
 cat <<EOF > nlk/config-map.yaml
@@ -37,7 +35,7 @@ kubectl apply -f nic/install/ingress-class.yaml
 export jwt_token=$(cat nic/install/nginx-repo.jwt)
 echo $jwt_token
 kubectl create secret docker-registry regcred --docker-server=private-registry.nginx.com --docker-username=$jwt_token --docker-password=none -n nginx-ingress
-kubectl apply -f install/nginx-plus-ingress.yaml
+kubectl apply -f nic/install/nginx-plus-ingress.yaml
 
 echo "Installing cafe app..."
 kubectl apply -f nginx-loadbalancer-kubernetes/docs/cafe-demo/cafe-secret.yaml
@@ -49,15 +47,16 @@ echo "Installing NLK"
 export PLUS_IP=$(docker network inspect kind | grep -o '"Name": "nginx-plus"' -A 5 | grep '"IPv4Address":' | cut -d '"' -f 4 | sed 's/\/16//')
 sed -i "" "s/PLUS_IP/$PLUS_IP/g" ./nlk/config-map.yaml
 
+echo "PLUS_IP is $PLUS_IP"
 
 kubectl apply -f nginx-loadbalancer-kubernetes/deployments/deployment/namespace.yaml
 ./nginx-loadbalancer-kubernetes/deployments/rbac/apply.sh
 kubectl apply -f nlk/config-map.yaml
 kubectl apply -f nginx-loadbalancer-kubernetes/deployments/deployment/deployment.yaml
 
-
 echo "Adding NodePort"
+sleep 10
+kubectl apply -f nlk/nodeport.yaml
 
 echo "Done!"
 echo "Next steps:\n    1. Check the NGINX Plus Dashboard at http://localhost:9000/dashboard\n    2. Make sure your /etc/hosts has the entry '127.0.0.1 cafe.example.com'\n    3. Try to hit the services 'curl -H -i -k https://cafe.example.com/tea'"
-kubectl apply -f nlk/nodeport.yaml
